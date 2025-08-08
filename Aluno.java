eb34b60d941f0285ff03755d242ddee098107dce
SamCalebe <samuel.calebe@academico.ifpb.edu.br>
2025-08-08 12:57:51 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,40 @@
+import java.util.Objects;
+
+public class Aluno {
+    private String nome;
+    private String matricula;
+
+    public Aluno(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Aluno)) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(matricula, aluno.matricula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(matricula);
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (Matrícula: " + matricula + ")";
+    }
+}
+
+
+
